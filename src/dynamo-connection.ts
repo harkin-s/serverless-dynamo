45caeffeb89cafd0cff284b5a12ec5917e81f0f3
sean harkin <sharkin104@gmail.com>
2020-05-11 20:25:03 +0100

    hard cond remote connection

--- src/dynamo-connection.ts
+++ src/dynamo-connection.ts
@@ -1,12 +1,15 @@
 import { DynamoDB } from "aws-sdk"
 import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
 
+
+//TODO Add remote connection config
 let db = null
 export async function getConnection(): Promise<DocumentClient>{
 
     if(db === null){
         db = new DynamoDB.DocumentClient({
-            endpoint: "http://localhost:8000"
+            // endpoint: "http://localhost:8000"
+            region: "eu-west-1"
         });
     } 
         
