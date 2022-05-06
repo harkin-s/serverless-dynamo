c47e129a912c8da6aaecc68ca1bdb4c1a4c84288
sean harkin <sean.harkin_ext@scout24.com>
2022-05-06 19:20:41 +0100

    add list all

--- src/list-tasks.ts
+++ src/list-tasks.ts
@@ -4,35 +4,65 @@ import { tableName } from "./config";
 
 export const list: APIGatewayProxyHandler = async (event, _context) => {
 
-    const {creator = null} = event.queryStringParameters;
-    const {statusCode, body} = await listTasks(creator);
-    
+    const { creator = null } = event.queryStringParameters;
+    const { statusCode, body } = creator ? await listTasks(creator) : await listAllTasks()
+
     return {
         statusCode,
         body
     };
 }
 
-async function listTasks(creator: string){
-    
-    try{
+async function listTasks(creator: string) {
+
+    try {
 
         const db = await getConnection();
         const params = {
-            TableName : tableName,
-            KeyConditionExpression : 'creator = :cr',
-            ExpressionAttributeValues : {
-                ':cr' : creator     
+            TableName: tableName,
+            KeyConditionExpression: 'creator = :cr',
+            ExpressionAttributeValues: {
+                ':cr': creator
             }
         }
-        const {Items = []} = await db.query(params).promise();
-
+        const { Items = [] } = await db.query(params).promise();
         return {
             statusCode: 200,
             body: JSON.stringify(Items)
         }
-        
-    } catch(error){
+
+    } catch (error) {
+        console.error(error)
+        return {
+            statusCode: 500,
+            body: "Server error"
+        };
+
+    }
+}
+
+async function listAllTasks() {
+    try {
+        const db = await getConnection();
+        let params = { TableName: tableName, ExclusiveStartKey: null };
+        let items: any
+        let scanResults = [];
+
+        do {
+            items = await db.scan(params).promise();
+            items.Items.forEach((item: any) => scanResults.push(item));
+            params.ExclusiveStartKey = items.LastEvaluatedKey;
+
+        } while (items.LastEvaluatedKey !== undefined)
+
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(scanResults)
+        }
+
+
+    } catch (error) {
         console.error(error)
         return {
             statusCode: 500,
