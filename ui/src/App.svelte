6f00145abbfa65c322c29963377e729b21e381bf
sean harkin <sean.harkin_ext@scout24.com>
2022-05-09 20:34:13 +0100

    update headers

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -2,7 +2,9 @@
 	import { onMount } from "svelte";
 
 	onMount(async () => {
-		fetch("https://jsonplaceholder.typicode.com/todos/1'")
+		fetch(
+			"https://hw93br9xm1.execute-api.eu-west-1.amazonaws.com/live/list"
+		)
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
